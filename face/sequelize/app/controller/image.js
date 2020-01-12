1c4be05f499925be80ab902412ccad39fe21dee1
abser <abserari@gmail.com>
2020-01-12 17:23:23 +0800

    complete faceinfo

--- face/sequelize/app/controller/image.js
+++ face/sequelize/app/controller/image.js
@@ -2,57 +2,63 @@
 
 const Controller = require('egg').Controller;
 const Core = require('@alicloud/pop-core');
-const ec = require('emitter-io').connect()
-const groups = "workers"
-class ImageInfoController extends Controller {
-  async index() {
-    // 1. get image path
-    const ctx = this.ctx;
-    var id = 1
-    //  2. use aliyun api detect image person id
-    const client = new Core({
-      accessKeyId: 'LTAI4FcUTY6C6mReK4Eq8Bqz',
-      accessKeySecret: 'WWwUZiaub9v17retzjuF6AmxcuEJ38',
-      endpoint: 'https://face.cn-shanghai.aliyuncs.com', // 注意修改这里
-      apiVersion: '2018-12-03'
-    });
-    let params = {
-      "Group": groups,
-      // "Content": ctx.params.image
-      "ImageUrl": ctx.queries.imageurl[0]
-    }
 
-    const requestOption = {
-      method: 'POST'
-    };
-    // call aliyun api to get id from picture
-    client.request('RecognizeFace', params, requestOption).then(async (result) => {
-      // console.log(result)
+var emitterKey = "DdYjd8w_zH4UTj3OLWOqM8kSmbk9c68H";
+var channel = "personinfo";
+
+const groups = "workers"
+
+async function face(ctx, params) {
+  const client = new Core({
+    accessKeyId: 'LTAI4FcUTY6C6mReK4Eq8Bqz',
+    accessKeySecret: 'WWwUZiaub9v17retzjuF6AmxcuEJ38',
+    endpoint: 'https://face.cn-shanghai.aliyuncs.com', // 注意修改这里
+    apiVersion: '2018-12-03'
+  });
+
+  const requestOption = {
+    method: 'POST'
+  }
+
+  client.request('RecognizeFace', params, requestOption).then(async (result) => {
+      console.log( result.Data[0].score)
+      // if (result.Data.score < 0.4) {
+      //   return
+      // }
       // 3. get personinfo from person id
-      console.log(id)
     
       const userinfo = await ctx.service.user.find(ctx.helper.parseInt(result.Data[0].person));
-
+      // console.log( userinfo.dataValues)
       // publish a message to the chat channel
+      const ec = require('emitter-io').connect({host:"127.0.0.1", port:"8080"})
       ec.publish({
-        key: "ms4GqyLupd4yrJqBciqKnJreS7xNrWWx",
-        channel: "personinfo",
-        message: userinfo
+        key: emitterKey,
+        channel: channel,
+        message: JSON.stringify(userinfo.dataValues)
       });
 
     }, (ex) => {
       console.log(ex);
     })
+}
 
-    ec.subscribe({
-      key: "ms4GqyLupd4yrJqBciqKnJreS7xNrWWx",
-      channel: "personinfo"
-    });
-    // on every message, print it out
-    ec.on('message', function (msg) {
-      console.log(msg.asString());
-    });
+class ImageInfoController extends Controller {
+  async index() {
+    // 1. get image path
+    const ctx = this.ctx;
 
+    // console.log("body" + ctx.request.body.imageurl.length)
+
+    for (let i = 0 ; i< ctx.request.body.imageurl.length ; i++) {
+      let params = {
+        "Group": groups,
+        "Content": ctx.request.body.imageurl[i]
+        // "ImageUrl": ctx.queries.imageurl[0]
+      }
+
+      await face(ctx, params)
+    }
+    
     // 4. notify caller wait for info on emitter channel
     ctx.status = 200;
     ctx.body = {
