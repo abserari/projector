916abf6ada67888f152f39cefe209a7f277b4885
yangchengkai1 <1351599172@qq.com>
2020-01-15 21:36:02 +0800

    update input page

--- dashboard/frontend/src/pages/EmptyPage/InputBasic/index.jsx
+++ dashboard/frontend/src/pages/EmptyPage/InputBasic/index.jsx
@@ -1,37 +1,58 @@
-import React, { useState } from 'react';
-import { Input } from 'antd';
+import React from 'react';
+import { Input,Button } from 'antd';
 import styles from './index.less';
 
-export default () => {
-  
+let state = {
+  pm: 0,
+  tmp: 0,
+  hum:0
+}
 
-  const getvalue = (value) => {
-    const emitterKey = 'MMfLon7iAZaQH-_6sFtauuzikqGQaQXO';
-    const channel = 'personinfo';
-  
-    const client = require('emitter-io').connect({
-      host: '127.0.0.1',
-      port: '8080',
-    });
-  
-    client.publish({
-      key: emitterKey,
-      channel: channel,
-      message: value
-    })
+export default () => {
+  const changePm = (value) => {
+    state.pm = value
+  }
+  const changeTmp = (value) => {
+    state.tmp = value
+  }
+  const changeHum = (value) => {
+    state.hum = value
   }
 
-  return ( 
+  const publish = value => {
+    const client = require('emitter-io').connect({ host: '127.0.0.1', port: '8080' });
+
+    client.publish({
+      key: 'Ws8wxZjTP9GbEbncf8FYCHr_volK1Bbu',
+      channel: 'pine',
+      message: JSON.stringify(value),
+    });
+  };
+
+  return (
     <div className={styles.container}>
-      <div id="components-input-demo-basic" >
-        <Input.Search
-        placeholder="input" 
-        // onPressEnter={e => pub(e.target.value)}
-        enterButton="提交"
-        onSearch={value => getvalue(value)}
-        addonBefore="湿度"
-        />
+      <div id="components-input-demo-basic">
+      <Input.Group>
+          <Input
+            addonBefore="PM2.5"
+            onChange={e => changePm(e.target.value)}
+          />
+          <p></p>
+          <Input
+            addonBefore="温度"
+            onChange={e => changeTmp(e.target.value)}
+          />
+          <p></p>
+          <Input
+            addonBefore="湿度"
+            onChange={e => changeHum(e.target.value)}
+          />
+          <p></p>
+          <Button type="primary" onClick={() => publish(state)}>
+           提交
+          </Button>
+      </Input.Group>
       </div>
     </div>
-    );
+  );
 };
--- dashboard/frontend/src/pages/EmptyPage/index.jsx
+++ dashboard/frontend/src/pages/EmptyPage/index.jsx
@@ -3,40 +3,19 @@ import React, { useState, useEffect } from 'react';
 import { Spin } from 'antd';
 import styles from './index.less';
 import InputBasic from './InputBasic';
-import Temperature from './Temperature';
-import Pmtwopfive from './Pmtwopfive';
-
 export default () => {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-
-    const emitterKey = "MMfLon7iAZaQH-_6sFtauuzikqGQaQXO";
-    const channel = "personinfo";
-
-    const pub = (value) => {
-      const client = require('emitter-io').connect({host:"127.0.0.1", port:"8080"}); 
-  
-      client.publish({
-        key: emitterKey,
-        channel: channel,
-        message: value
-      })
-    }
-
     setTimeout(() => {
       setLoading(false);
     }, 3000);
   }, []);
   return (
     <PageHeaderWrapper className={styles.main}>
-      <Temperature />
-      <br />
       <InputBasic />
-      <br />
-      <Pmtwopfive />
       <div
         style={{
-          paddingTop: 200,
+          paddingTop: 100,
           textAlign: 'center',
         }}
       >
